2073c54d06d12fe619612a248b71d6fde2a4749a
Cyronp <vitorhenriquedasilveira@gmail.com>
2026-03-06 23:55:38 -0300

    feat: add header to layout.tsx, making so every page has the same header.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
+import { Header } from "@/components/header";
+
+import { Fustat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const fustat = Fustat({
+  variable: "--font-fustat",
   subsets: ["latin"],
 });
 
@@ -24,9 +22,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${fustat.variable} font-sans antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
---
+++ components/header.tsx
@@ -0,0 +1,95 @@
+import Link from "next/link";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { Button } from "./ui/button";
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background">
+      <div className="flex h-16 w-full items-center justify-between px-14">
+        <div className="font-bold text-xl">RecycleBin</div>
+        <div className="flex text-base font-medium">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link href="/">Home</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link href="/">Instruções</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link href="/">Pontos de Coleta</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Denuncia</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="w-96">
+                    <ListItem href="/" title="Realizar solicitação de Denuncia">
+                      Acessar o registro de denuncia de má conduta.
+                    </ListItem>
+                    <ListItem href="/" title="Visualizar/Editar Denuncia">
+                      Acessar a visualização ou edição de denuncias realizadas
+                      por você.
+                    </ListItem>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Coleta</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="w-96">
+                    <ListItem href="/" title="Realizar solicitação de Coleta">
+                      Acessar o registro de coleta de materiais.
+                    </ListItem>
+                    <ListItem href="/" title="Visualizar/Editar Coleta">
+                      Acessar a visualização ou edição de solicitações de
+                      coletas realizadas por você.
+                    </ListItem>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div className="flex">
+          <Button variant="outline" size="lg" className="cursor-pointer">
+            Login
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function ListItem({
+  title,
+  children,
+  href,
+  ...props
+}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
+  return (
+    <li {...props}>
+      <NavigationMenuLink asChild>
+        <Link href={href}>
+          <div className="flex flex-col gap-1 text-sm">
+            <div className="leading-none font-medium">{title}</div>
+            <div className="line-clamp-2 text-muted-foreground">{children}</div>
+          </div>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+}
